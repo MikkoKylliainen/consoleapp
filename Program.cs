df16a187ac36a015b4fe7f3980fe3d972d283a38
Mikko Kylliäinen <mikko.kylliainen@gmail.com>
2022-09-13 15:34:11 +0300

    namethingy

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int age;
+string fname;
+
+Console.WriteLine("Name?");
+fname=Console.ReadLine();
+
+Console.WriteLine("Hello "+fname);
