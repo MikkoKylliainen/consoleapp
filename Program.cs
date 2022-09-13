dfcfe0fa3d99e281af752248e341c436bbc7479f
Mikko Kylliäinen <mikko.kylliainen@gmail.com>
2022-09-13 15:46:51 +0300

    update

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int age;
 string fname;
+int byear;
 
 Console.WriteLine("Name?");
 fname=Console.ReadLine();
 
 Console.WriteLine("Hello "+fname);
+
+Console.WriteLine("jotain");
+
+int.TryParse(Console.ReadLine(), out age);
+
+byear=2022-age;
+Console.WriteLine("yea " +byear);
